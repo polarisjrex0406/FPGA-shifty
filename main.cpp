d155c43be9307c624033cef910d056c76b9aa476
polarisjrex0406 <zeppelix0406@gmail.com>
2021-10-09 17:59:51 -0500

    c++ idioms

--- main.cpp
+++ main.cpp
@@ -21,13 +21,12 @@ int main(int argc, const char **argv) {
     char password_buf[11] = { 0 };
     char password[12] = { 0 };
     (void) strncpy(hash_buf, argv[1], sizeof(hash_buf));
-    auto *top = new Vshifty{"top"};
-    (void) std::memcpy(top->hash, hash_buf, sizeof(hash_buf));
-    top->eval();
-    (void) std::memcpy(password_buf, top->password, sizeof(password_buf));
+    Vshifty top{"top"};
+    (void) std::memcpy(top.hash, hash_buf, sizeof(hash_buf));
+    top.eval();
+    (void) std::memcpy(password_buf, top.password, sizeof(password_buf));
     (void) strncpy(password, password_buf, 11);
     password[strlen(hash_buf)/2 - 1] = '\0';
     std::cout << password << std::endl;
-    delete top;
     return EXIT_SUCCESS;
 }
